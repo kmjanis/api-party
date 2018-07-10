fa2b13c949061b372225d56ce4fcb71a468686dd
Kelly Janis <kellymj98@gmail.com>
2018-07-10 15:50:21 -0400

    css and create github component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, NavLink, Switch } from 'react-router-dom'
+
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">API party</h1>
+        <header>
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
+        </header>
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/github" render={() => <h1>GitHub!</h1>} />
+          <Route render={() => (
+              <p>To get started, click one of the links above.</p>
+          )} />
+        </Switch>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
